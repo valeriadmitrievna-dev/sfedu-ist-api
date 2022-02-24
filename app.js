8a6c867975f8e546baea510a2c1e81c72da3eb25
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2022-02-24 21:01:11 +0300

    Update cors headers

--- app.js
+++ app.js
@@ -18,10 +18,14 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(fileupload());
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", process.env.app);
   res.setHeader("Origin", process.env.app);
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length, Authorization"
+  );
   next();
 });
 
